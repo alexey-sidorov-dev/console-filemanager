21f101d877efd9dfb80ea27cdcfa16acac5e521b
Alexey Sidorov <alexey.sidorov.dev@mail.ru>
2025-02-05 23:57:36 +0300

    add project

---
+++ src/main/java/ru/otus/java/basic/project/Application.java
@@ -0,0 +1,8 @@
+package ru.otus.java.basic.project;
+
+public class Application {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
---
+++ src/test/java/ru/otus/java/basic/project/ApplicationTest.java
@@ -0,0 +1,11 @@
+package ru.otus.java.basic.project;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+  @Test
+  void testMain() {
+    Application.main(new String[]{"args"});
+  }
+}
